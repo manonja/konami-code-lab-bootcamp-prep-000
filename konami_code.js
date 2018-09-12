62c8f63297dc9489f48c22f2a0a6cc450ccca963
manon jacquin <manon.jacquin@gmail.com>
2018-09-12 16:03:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -29,7 +29,7 @@ function triggerKeyDown(key) {
 // This is the function that would be invoked by the event listener.
 function onKeyDownHandler(e) {
   const key = e.key;
-   for (let i = 0, l = codes.length; i < l; i++) {
+   for (let i = 0, i < codes.length; i++) {
         triggerKeyDown(codes[i]);
         if (key === codes[index]) {
     index++;

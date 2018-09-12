cb268f8332ba8202c4eb2896a0754d12f982fd6b
manon jacquin <manon.jacquin@gmail.com>
2018-09-12 15:54:04 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -20,17 +20,12 @@ let index = 0;
 // This is the function that would be invoked by the event listener.
 function onKeyDownHandler(e) {
   const key = e.key;
+   for (let i = 0, l = codes.length; i < l; i++) {
+        triggerKeyDown(codes[i]);
+        
+        
+      }
 
-  if (key === codes[index]) {
-    index++;
-
-    if (index === codes.length) {
-      alert("Hurray!");
-
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
+  
 }
 }
